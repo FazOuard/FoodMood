7c384af27ebdf38d1c76860d9705d470360a6eb9
ayairis <ayaalae12.2002@gmail.com>
2024-12-25 14:15:49 +0100

    NavBar

--- frontend/src/pages/plat_info/plat_info.jsx
+++ frontend/src/pages/plat_info/plat_info.jsx
@@ -4,6 +4,7 @@ import './plat_info.css'
 import { useLocation, useNavigate } from 'react-router-dom';
 import RandomPlats from '../../components/randomPlats/randomPlats';
 import Plat from '../../components/plat/plat';
+import NavBar from '../../components/navbar/navbar';
 
 const Plat_info = () => {
 
@@ -12,6 +13,7 @@ const Plat_info = () => {
 
     return (
         <div className='plat_info'>
+            <NavBar/>
             <div className='plat_info1'>
                 <div><RandomPlats /></div>
                 <div><Plat/></div>
