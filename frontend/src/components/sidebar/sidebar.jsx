a8b86f1b9a072aa3da38dbbf5b1ce538b8455c96
ayairis <ayaalae12.2002@gmail.com>
2024-12-25 16:05:52 +0100

    recommand

--- frontend/src/components/sidebar/sidebar.jsx
+++ frontend/src/components/sidebar/sidebar.jsx
@@ -1,18 +1,39 @@
 import React, { useEffect, useState } from 'react';
 import './sidebar.css'
+import profile from '../../assets/icons/profile.png'
+import calendar from '../../assets/icons/calendar.png'
+import stat from '../../assets/icons/stat.png'
+import dish from '../../assets/icons/dish.png'
+import magic from '../../assets/icons/magic.png'
 
 const SideBar = () => {
     
     return (
         <div className='sidebar-all'>
             <div className='sidebar-choices1'>
-                <h4>Tout nos plats</h4>
+                <div className='sidebar-recom'>
+                    <img src={magic} />
+                    <h4>Recommander</h4>
+                </div>
+                <div className='sidebar-choice'>
+                    <img src={dish} />
+                    <h4>Tout nos plats</h4>
+                </div>
                 <div className='sidebar-line'/>
-                <h4>Mes statistiques</h4>
+                <div className='sidebar-choice'>
+                    <img src={stat} />
+                    <h4>Mes statistiques</h4>
+                </div>
                 <div className='sidebar-line'/>
-                <h4>Plannifier</h4>
+                <div className='sidebar-choice'>
+                    <img src={calendar} />
+                    <h4>Plannifier</h4>
+                </div>
                 <div className='sidebar-line'/>
-                <h4>Mon profile</h4>
+                <div className='sidebar-choice'>
+                    <img src={profile} />
+                    <h4>Mon profile</h4>
+                </div>
             </div>
             <div className='sidebar-choices2'>
                 <div className='sidebar-line'/>
