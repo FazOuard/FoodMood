782e47abc9c798b3dfc6e79788f6d98087721f53
ayairis <ayaalae12.2002@gmail.com>
2024-12-26 20:47:51 +0100

    Small Fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import './App.css';
 import Recommendation from "./pages/recommendation.jsx";
 import Plat_info from './pages/plat_info/plat_info.jsx';
 import Plat_All from './pages/plats/plats.jsx';
-import NbrPerson from './components/nbrPerson/nbrPerson.jsx';
 
 function App() {
 
@@ -15,8 +14,6 @@ function App() {
       <Route path="/" element={<Recommendation />} />
       <Route path="/plats" element={<Plat_All />} />
       <Route path="/platinfo" element={<Plat_info />} />
-      <Route path="/nbrperson" element={<NbrPerson value={1}/>} />
-      
       </Routes>
     </Router>
     </div>
