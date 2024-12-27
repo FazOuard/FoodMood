786f440e3228b24b1fd03e623c9fb10dc4e183cc
ayairis <ayaalae12.2002@gmail.com>
2024-12-27 22:14:29 +0100

    plan week & month

--- frontend/src/pages/planifier/planifier.jsx
+++ frontend/src/pages/planifier/planifier.jsx
@@ -5,6 +5,7 @@ import NavBar from '../../components/navbar/navbar';
 import SideBar from '../../components/sidebar/sidebar';
 import todo from '../../assets/3Drender/todo.png'
 import weekcalendar from '../../assets/3Drender/weekcalendar.png'
+import check from '../../assets/icons/check.png'
 
 
 const Planifier = () => {
@@ -20,10 +21,48 @@ const Planifier = () => {
                 <div className='planifier-card'>
                     <h2>Planifier une semaine</h2>
                     <img src={weekcalendar} />
+                    <div className='plan-line'/>
+                    <div className='plan-check'>
+                        <div className='plan-check-one'>
+                            <img src={check}/>
+                            <h4>Plannifier les plats d'une semaine.</h4>
+                        </div>
+                        <div className='plan-check-one'>
+                            <img src={check}/>
+                            <h4>Optimiser vos courses.</h4>
+                        </div>
+                        <div className='plan-check-one'>
+                            <img src={check}/>
+                            <h4>Avoir un bilan de votre semaine.</h4>
+                        </div>
+                        <div className='plan-check-one'>
+                            <img src={check}/>
+                            <h4>Estimer votre budget.</h4>
+                        </div>
+                    </div>
                 </div>
                 <div className='planifier-card'>
                     <h2>Planifier un mois</h2>
                     <img src={todo} />
+                    <div className='plan-line'/>
+                    <div className='plan-check'>
+                        <div className='plan-check-one'>
+                            <img src={check}/>
+                            <h4>Plannifier les plats d'un mois.</h4>
+                        </div>
+                        <div className='plan-check-one'>
+                            <img src={check}/>
+                            <h4>Optimiser vos courses.</h4>
+                        </div>
+                        <div className='plan-check-one'>
+                            <img src={check}/>
+                            <h4>Avoir un bilan de votre mois.</h4>
+                        </div>
+                        <div className='plan-check-one'>
+                            <img src={check}/>
+                            <h4>Estimer votre budget.</h4>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
