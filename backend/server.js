515110a2d6c8cc12ec7d2d80037c83aaa205bdc3
ayairis <ayaalae12.2002@gmail.com>
2025-01-29 17:36:00 +0100

    fix merge

--- backend/server.js
+++ backend/server.js
@@ -4,16 +4,14 @@ import plat from './routers/plat.js'
 import ingredientsRoutes from './routers/ingredientsRoutes.js';
 import platsIngRoutes from './routers/platsIngRoutes.js';
 import router from "./routers/auth.js";
-<<<<<<< HEAD
+
 import ingPlatRoutes from "./routers/ingPlatRoutes.js";
 import routerr from "./routers/ajouter.js";
-=======
 
 import ingPlatRoutes from "./routers/ingPlatRoutes.js";
 import userPreferences from "./routers/userPreferenceRouters.js"
 import IngGroupPlatRoutes from "./routers/IngGroupPlatRoutes.js"
 
->>>>>>> fd0008c10e95e1e708bfc0f0a0ced2118f066cf6
 
 const app = express();
 app.use(cors()); 
@@ -22,13 +20,10 @@ app.use('/data', plat);
 app.use('/ingredients', ingredientsRoutes);
 app.use('/platsIng', platsIngRoutes);
 app.use('/platIng', ingPlatRoutes);
-<<<<<<< HEAD
 app.use('/addDish', ingPlatRoutes);
-=======
 app.use('/userPreference', userPreferences);
 
 app.use('/IngGroupPlat', IngGroupPlatRoutes);
->>>>>>> fd0008c10e95e1e708bfc0f0a0ced2118f066cf6
 
 app.use("/api/auth", router);
 app.use("/api/ajouter", routerr);
