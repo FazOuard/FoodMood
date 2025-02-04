f2aa45fd6cd2e3e4c87284a14a6ebf8adbfe7480
ayairis <ayaalae12.2002@gmail.com>
2025-02-04 20:09:54 +0100

    Morocco

---
+++ frontend/api/moroccoData.js
@@ -0,0 +1,27 @@
+import IMG1 from "../src/assets/map/Regions/IMG1.png"
+import IMG2 from "../src/assets/map/Regions/IMG2.png"
+import IMG3 from "../src/assets/map/Regions/IMG3.png"
+import IMG4 from "../src/assets/map/Regions/IMG4.png"
+import IMG5 from "../src/assets/map/Regions/IMG5.png"
+import IMG6 from "../src/assets/map/Regions/IMG6.png"
+import IMG7 from "../src/assets/map/Regions/IMG7.png"
+import IMG8 from "../src/assets/map/Regions/IMG8.png"
+import IMG9 from "../src/assets/map/Regions/IMG9.png"
+import IMG10 from "../src/assets/map/Regions/IMG10.png"
+import IMG11 from "../src/assets/map/Regions/IMG11.png"
+import IMG12 from "../src/assets/map/Regions/IMG12.png"
+
+export const MoroccoData = [
+    { name: "Tanger-Tétouan-Al Hoceima", img: IMG1},
+    { name: "Souss-Massa", img: IMG2},
+    { name: "Rabat-Salé-Kenitra", img: IMG3},
+    { name: "Oriental", img: IMG4},
+    { name: "Marrakech-Safi", img: IMG5},
+    { name: "Laayoune-Sakia el Hamra", img: IMG6},
+    { name: "Guelmim-Oued Noun", img: IMG7},
+    { name: "Fès-Meknès", img: IMG8},
+    { name: "Deraa-Tafilalet", img: IMG9},
+    { name: "Dakhla-Oued ed Dahab", img: IMG10},
+    { name: "Casablanca-Settat", img: IMG11},
+    { name: "Béni Mellal-Khénifra", img: IMG12},
+]
